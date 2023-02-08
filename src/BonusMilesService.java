e3bcceaad2cefb016dfe8eb05502cdbaeb8c3cc9
Dani1899 <120176482+Dani1899@users.noreply.github.com>
2023-02-09 00:25:04 +0300

    Update BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,6 @@
 public class BonusMilesService {
     public int calculate(int price) {
         int costForOneMile = 20;//Количество рублей для одной бонусной мили
-        int bonus = price / costForOneMile;//количество бонусных миль
-        return bonus;
+        return price / costForOneMile;//количество бонусных миль
     }
 }
